5fae99abb205968d6012aab836980fd0219549ba
Truong Anh Vu <122198260+truonganhvu205@users.noreply.github.com>
2023-12-14 03:08:40 +0700

    Delete littlelemon directory

--- littlelemon/littlelemon/tests/test-models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from restaurant.models import MenuItems
-
-class MenuTest(TestCase):
-    def test_get_item(self):
-        item = MenuItems.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(item, "IceCream : 80")
