f66131568dfa0634ab2f95b8364f56408fd53cbf
vu <truonganhvu205@gmail.com>
2023-12-27 10:52:47 +0700

    update

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import MenuItems, Booking
 
 # Register your models here.
-admin.site.register(Booking)
 admin.site.register(MenuItems)
+admin.site.register(Booking)
